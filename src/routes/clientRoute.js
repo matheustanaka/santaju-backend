585b9d0a666b1c32e592956334f871d779a74534
matheustanaka <matheustanakanog@gmail.com>
2023-01-13 20:30:20 -0300

    feat: add put and delete routes

--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -3,10 +3,13 @@ const routes = express.Router();
 const {
   createClient,
   listAllClients,
+  updateClient,
+  deleteClient,
 } = require("../controllers/clientController");
 
 routes.post("/api/client", createClient);
 routes.get("/api/clients", listAllClients);
-routes.put("/api/clients/:id");
+routes.put("/api/client/:id", updateClient);
+routes.delete("/api/client/:id", deleteClient);
 
 module.exports = routes;
