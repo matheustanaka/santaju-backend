cdc90b948f0b96dc88c6ff050bb531adcb4039e2
matheustanaka <matheustanakanog@gmail.com>
2023-01-13 20:30:36 -0300

    feat: add update and delete controller

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -31,3 +31,32 @@ exports.listAllClients = async (req, res) => {
     res.status(400).json(error);
   }
 };
+
+exports.updateClient = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedClient = await prisma.client.update({
+      where: { id: String(id) },
+      data: {
+        ...req.body,
+      },
+    });
+
+    res.status(200).json(updatedClient);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+exports.deleteClient = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedClient = await prisma.client.delete({
+      where: { id: String(id) },
+    });
+
+    res.status(200).json(deletedClient);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
