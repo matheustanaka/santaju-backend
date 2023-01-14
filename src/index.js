fe8fa48a3d9d119df066462cb6e393c24363a65f
matheustanaka <matheustanakanog@gmail.com>
2023-01-13 23:24:27 -0300

    fix: change product routes name

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const routes = require("../src/routes/clientRoute");
+const productRoutes = require("../src/routes/productRoute");
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(productRoutes);
 
 const port = process.env.PORT || "3000";
 
