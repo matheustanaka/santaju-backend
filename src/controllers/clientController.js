f062564f4254447ac17b0f1836bf6e8a7f697af1
Matheus Tanaka <matheustanakanog@gmail.com>
2023-01-20 09:38:39 -0300

    feat: add controller to filter one client by id

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -32,6 +32,22 @@ exports.listAllClients = async (req, res) => {
   }
 };
 
+exports.listOneClient = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const listClients = await prisma.client.findUnique({
+      where: { id: String(id) },
+      include: {
+        products: true,
+      },
+    });
+
+    res.status(200).json(listClients);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
 exports.updateClient = async (req, res) => {
   try {
     const { id } = req.params;
