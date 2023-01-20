d0c1efd9b07cc3284921c6636ff56d2adc9df9dd
Matheus Tanaka <matheustanakanog@gmail.com>
2023-01-20 09:38:27 -0300

    feat: add route to list one client by id

--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -3,12 +3,14 @@ const routes = express.Router();
 const {
   createClient,
   listAllClients,
+  listOneClient,
   updateClient,
   deleteClient,
 } = require("../controllers/clientController");
 
 routes.post("/api/client", createClient);
 routes.get("/api/clients", listAllClients);
+routes.get("/api/client/:id", listOneClient);
 routes.put("/api/client/:id", updateClient);
 routes.delete("/api/client/:id", deleteClient);
 
