c470f4d33154f53ae24498332e4206fe666275b8
matheustanaka <matheustanakanog@gmail.com>
2023-02-04 12:36:11 -0300

    fix: replace int id to string

--- prisma/migrations/20230202133411_init/migration.sql
+++ prisma/migrations/20230202133411_init/migration.sql
@@ -1,22 +1,22 @@
 -- CreateTable
 CREATE TABLE "clients" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "phone" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "products" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "id" TEXT NOT NULL PRIMARY KEY,
     "title" TEXT NOT NULL,
     "price" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "orders" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "id_product" INTEGER NOT NULL,
-    "id_client" INTEGER NOT NULL,
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "id_product" TEXT NOT NULL,
+    "id_client" TEXT NOT NULL,
     CONSTRAINT "orders_id_product_fkey" FOREIGN KEY ("id_product") REFERENCES "products" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT "orders_id_client_fkey" FOREIGN KEY ("id_client") REFERENCES "clients" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
